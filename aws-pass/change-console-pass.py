80dea5e3ddba5aade6fde6393eff4c12daaa6c9d
Piotr Jarosz <piotr.jarosz@icoqu.com>
2019-12-02 02:21:37 +0100

    Added changing password, session handling, quiet mode and some other things

--- aws-pass/change-console-pass.py
+++ aws-pass/change-console-pass.py
@@ -37,9 +37,11 @@ parser.add_argument(
   '--access-key', 
   help='AWS AccessKey', 
   default=False)
-
 parser.add_argument('--debug', action='store_true', 
   help=argparse.SUPPRESS, default=False
+  )
+parser.add_argument('--quiet', '-q', action='store_true', 
+  help='Do not ask about user input. Assume as "yes"', default=False
   )
 
 args = parser.parse_args()
@@ -51,7 +53,10 @@ def log(message):
 
 
 if args.old_password == None:
-  args.old_password = input('Please, provide your old password:')
+  if not args.quiet:
+    args.old_password = input('Please, provide your old password:')
+  else:
+    args.old_password = ''
 
 if args.old_password == '':
   print('You need to provide your old passord.')
@@ -79,5 +84,20 @@ if args.profile:
 elif args.access_key_id and args.access_key:
   session = boto3.Session(aws_access_key_id=args.access_key_id, aws_secret_access_key=args.access_key)
 
-client = session.client('iam')
-
+client = session.client('sts')
+acc_id = client.get_caller_identity().get('Account')
+username = client.get_caller_identity().get('Arn').split('/')[-1]
+if not args.quiet:
+  answer = ""
+  while answer not in ["y", "n"]:
+    answer = input("Are you sure you want to change password for " + username + " on account " + acc_id + " [Y/N]? ").lower()
+else:
+  answer = "y"
+if answer == "y":
+  client = session.client('iam')
+  response = client.change_password(
+            OldPassword=args.old_password,
+            NewPassword=args.new_password
+        )
+else:
+  print('Ok, maybe You will try later?')
